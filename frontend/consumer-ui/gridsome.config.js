186fe34de607c9b4a489c94050cbfa6cf3c7c36c
Laurent Michel <laurent.michel@ruag.com>
2019-12-06 18:09:17 +0100

    use CONSUMER_UI_HOST / PORT and GRAPHQL_API in consumer ui configuration

--- frontend/consumer-ui/gridsome.config.js
+++ frontend/consumer-ui/gridsome.config.js
@@ -6,12 +6,12 @@
 
 module.exports = {
   siteName: 'La Budzonnerie',
-  host: 'localhost',
+  host: process.env.CONSUMER_UI_HOST || 'localhost',
   port: process.env.CONSUMER_UI_PORT || 4000,
   plugins: [{
     use: '@gridsome/source-graphql',
     options: {
-      url: 'http://localhost:8080/v1/graphql',
+      url: process.env.GRAPHQL_API || 'http://localhost:8080/v1/graphql/',
       fieldName: 'hasura',
       headers: {
         // Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
