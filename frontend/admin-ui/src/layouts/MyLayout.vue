43a9b933e1529298332dd0d78714dce40d83cd61
Laurent Michel <laurent.michel@ruag.com>
2019-12-06 08:40:37 +0100

    eslint fix in-place

--- frontend/admin-ui/src/layouts/MyLayout.vue
+++ frontend/admin-ui/src/layouts/MyLayout.vue
@@ -29,7 +29,12 @@
         <q-item-label header>
           Essential Links
         </q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://quasar.dev"
+        >
           <q-item-section avatar>
             <q-icon name="school" />
           </q-item-section>
@@ -40,7 +45,12 @@
             </q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.quasar.dev">
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://github.quasar.dev"
+        >
           <q-item-section avatar>
             <q-icon name="code" />
           </q-item-section>
@@ -51,7 +61,12 @@
             </q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://chat.quasar.dev"
+        >
           <q-item-section avatar>
             <q-icon name="chat" />
           </q-item-section>
@@ -62,7 +77,12 @@
             </q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://forum.quasar.dev"
+        >
           <q-item-section avatar>
             <q-icon name="record_voice_over" />
           </q-item-section>
@@ -73,7 +93,12 @@
             </q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://twitter.quasar.dev">
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://twitter.quasar.dev"
+        >
           <q-item-section avatar>
             <q-icon name="rss_feed" />
           </q-item-section>
@@ -84,7 +109,12 @@
             </q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://facebook.quasar.dev">
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://facebook.quasar.dev"
+        >
           <q-item-section avatar>
             <q-icon name="public" />
           </q-item-section>
